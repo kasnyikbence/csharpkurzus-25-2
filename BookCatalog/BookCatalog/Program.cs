574fd45e8c0fa3e83164a0ba54038b49442c2fc7
Kasnyik Bence <h263908@stud.u-szeged.hu>
2025-11-11 14:45:23 +0100

    Create Book Catalog project

---
+++ BookCatalog/BookCatalog/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookCatalog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
